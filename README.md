927122a31fa59a508a371a6caaf59d3dec1f1168
RizkyAdi487 <rizkyadi487@gmail.com>
2019-04-26 21:22:08 +0700

    Add : FastRunMaker, FormatChecker

--- README.md
+++ README.md
@@ -3,7 +3,5 @@ Add Program that can called from Run...
 
 ## Usage
 1. Open executable location using `Command Prompt`
-2. Type `RunIt.exe [Key] "[Path of your application]"` dont forget using double quotes (") for path
-
-## Future work
-Make this program can fast add just drag and drop the application to create new key
+2. Type `RunIt.exe [Key] "[Path of your application]"` don't forget using double quotes (") for path
+or you can just drag and drop your application
--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/sys/windows/registry"
 )
 
 func main() {
-	keyName := os.Args[1] + ".exe"
-	keyPath := os.Args[2]
+	var keyName string
+	var keyPath string
+	if len(os.Args) == 2 {
+		filename, format := checker(os.Args[1])
+		if format != ".exe" {
+			fmt.Println("Your format is", format, "we expecting .exe extension")
+			exitProgram("Please use Application that using .exe etension")
+		}
+		keyName = filename + format
+		keyPath = os.Args[1]
+	} else if len(os.Args) == 3 {
+		keyName = os.Args[1] + ".exe"
+		_, format := checker(os.Args[2])
+		if format != ".exe" {
+			fmt.Println("Your format is", format, "we expecting .exe extension")
+			exitProgram("Please use Application that using .exe etension")
+		}
+		keyPath = os.Args[2]
+	} else {
+		exitProgram("We need path")
+	}
+
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths`, registry.QUERY_VALUE|registry.SET_VALUE)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	nk, _, err := registry.CreateKey(k, keyName, registry.QUERY_VALUE|registry.SET_VALUE)
+	nk, exist, err := registry.CreateKey(k, keyName, registry.QUERY_VALUE|registry.SET_VALUE)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if exist {
+		fmt.Println("key already exist")
+	} else {
+		fmt.Println("Done")
 	}
 
 	if err := nk.SetStringValue("", keyPath); err != nil {
@@ -26,4 +53,17 @@ func main() {
 
 	defer k.Close()
 	defer nk.Close()
+}
+
+func checker(value string) (filename string, format string) {
+	file := filepath.Base(value)
+	format = filepath.Ext(value)
+	filename = file[0 : len(file)-len(format)]
+
+	return filename, format
+}
+
+func exitProgram(isi string) {
+	fmt.Println(isi)
+	os.Exit(1)
 }
